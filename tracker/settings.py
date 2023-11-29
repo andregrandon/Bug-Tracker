04383b72eae16a9ba11c993e73904bab5c91e149
Andre Grandon <andre@AndresMacStudio.lan>
2023-11-29 13:16:44 -0700

    push

--- tracker/settings.py
+++ tracker/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
-    'phonenumbers'
+    'phonenumbers',
     'accounts',
     'django-phonenumber-field',
     'static',
