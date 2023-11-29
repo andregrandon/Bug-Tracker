b5d2225de78ff067b751097d184fb7c80beef4b2
Andre Grandon <andre@AndresMacStudio.lan>
2023-11-29 13:11:41 -0700

    ggx

--- tracker/settings.py
+++ tracker/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3',
+    'phonenumbers'
     'accounts',
     'django-phonenumber-field',
     'static',
